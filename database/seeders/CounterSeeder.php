6e49f5147c966bbc00c60670b5bb849ba578f9f9
Henrique Queiroz <henrique.q.paula@gmail.com>
2023-10-20 23:42:02 -0300

    fix: ajustes finais

--- database/seeders/CounterSeeder.php
+++ database/seeders/CounterSeeder.php
@@ -14,40 +14,40 @@ class CounterSeeder extends Seeder
     public function run(): void
     {
         DB::table('counter')->insert([
-            [
-                'name' => 'Quack nuclear',
-                'description' => 'Semelhante a arma mais mortal da humanidade, a bomba "Quack nuclear" detonar todos os mortos-vivos do local, não há força que resista.',
-                'image' => '',
-                'weakness_id' => 1,
-            ],
+            // [
+            //     'name' => 'Quack nuclear',
+            //     'description' => 'Semelhante a arma mais mortal da humanidade, a bomba "Quack nuclear" detonar todos os mortos-vivos do local, não há força que resista.',
+            //     'image' => '',
+            //     'weakness_id' => 1,
+            // ],
             [
                 'name' => 'Bazooka de migalhas de pão',
                 'description' => 'Armamento pesado, 4kg de pão, polvora e muito ódio, todos os zumbis serão pulverizados.',
-                'image' => '',
+                'image' => 'bazooka.jpg',
                 'weakness_id' => 1,
             ],
             [
                 'name' => 'Granada de ovos',
                 'description' => 'Parecem inofensivas, mas seu estrago é grande, CUIDADO COM OS OVOS MEXIDOS!!',
-                'image' => '',
+                'image' => 'granada.jpg',
                 'weakness_id' => 2,
             ],
             [
                 'name' => 'Canhão de água',
                 'description' => 'Uma quantidade torrencial de água, projetada em alta velocidade, não deixa nenhum zumbi chegar perto, imagina se pega no olho',
-                'image' => '',
+                'image' => 'canhao.jgp',
                 'weakness_id' => 2,
             ],
-            [
-                'name' => 'Enxurrada de fake news',
-                'description' => 'São tantas informações absurdas e sem confirmação de fonte, que não tem jeito, qualquer cerebro travaria.',
-                'image' => '',
-                'weakness_id' => 3,
-            ],
+            // [
+            //     'name' => 'Enxurrada de fake news',
+            //     'description' => 'São tantas informações absurdas e sem confirmação de fonte, que não tem jeito, qualquer cerebro travaria.',
+            //     'image' => '',
+            //     'weakness_id' => 3,
+            // ],
             [
                 'name' => 'Katana de pena',
                 'description' => 'Um dialogo pode resolver muitas coisas, mas a violência com certeza encerra todas >:).',
-                'image' => '',
+                'image' => 'katana.jpg',
                 'weakness_id' => 3,
             ],
         ]);
--- database/seeders/DefenseSeeder.php
+++ database/seeders/DefenseSeeder.php
@@ -17,37 +17,37 @@ class DefenseSeeder extends Seeder
             [
                 'name' => 'Bolha de proteção',
                 'description' => 'Uma bolha de mágica de proteção, aguenta diversas pancadas, apara projéties, resumindo, um ótimo lugar para passar umas férias, 0 bugs, 0 problemas.',
-                'image' => '',
+                'image' => 'bolha.jpg',
                 'strength_id' => 1,
             ],
             [
                 'name' => 'Escudos de penas',
                 'description' => 'Penas geneticamente modificadas crescem rapidamente em torno do pato, sua resistência é similar ao aço, a defesa PERFEITA!',
-                'image' => '',
+                'image' => 'escudo.jpg',
                 'strength_id' => 1,
             ],
             [
                 'name' => 'Patas turbo',
                 'description' => 'Botas metálicas com propulsores a jato, não pega nuncaaa, coma nossa poeira.',
-                'image' => '',
+                'image' => 'turbo.jpg',
                 'strength_id' => 2,
             ],
             [
                 'name' => 'Voo de emêrgencia',
                 'description' => 'Fugir não deveria ser vergonha, é uma "saída estratégica", o voo é uma saída estratégica elegante.',
-                'image' => '',
+                'image' => 'voo.jpg',
                 'strength_id' => 2,
             ],
             [
                 'name' => 'Disfarce de zumbi',
                 'description' => 'Um pato zumbi... Serio, você atacaria um Pato zumbi? NÃO!! É o plano perfeito.',
-                'image' => '',
+                'image' => 'disfarce.jgp',
                 'strength_id' => 3,
             ],
             [
                 'name' => 'Dança frenética',
                 'description' => 'Uma série de passos de dança sensacionais, deslumbrantes e frenéticos, quando eles entenderem o que está acontecendo, o show já acabou.',
-                'image' => '',
+                'image' => 'danca.jpg',
                 'strength_id' => 3,
             ],
         ]);
